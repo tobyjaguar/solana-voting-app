4c0e3ee9bd605e3bd6fbe36c97be5246b02d250a
tobyjaguar <talgya@gmail.com>
2025-05-05 22:09:54 -0700

    remove .next

--- .next/static/chunks/app_page_tsx_037f488f._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_2b4c036f._.js",
-  "static/chunks/_2c8a2291._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/app_page_tsx_8808d446._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_2b4c036f._.js",
-  "static/chunks/_de908ec9._.js"
-],
-    source: "dynamic"
-});
